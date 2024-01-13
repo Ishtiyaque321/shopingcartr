a1254da4b93bb92e8cc99e3bc43a937b0bb85d10
Ishtiyaque Ahmad <istiyaqueahmad0@gmail.com>
2024-01-13 14:13:43 +0530

    first commit

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -11,13 +11,15 @@ export const PostCard = ({post, toggle, setToggle}) => {
       setToggle(!toggle);
     }
 
+    
+
   return (
     <div className="card">
         <p className="title">{title}</p>
         <p className="description">{description}</p>
         <p className="control">
-            <span className="author">{author.name}</span>
-            { isAuth && (author.id === auth.currentUser.uid) && <span onClick={handleDelete} className="delete"><i className="bi bi-trash3"></i></span> }
+            <span className="author">{author?.name}</span>
+            { isAuth && (author?.id === auth.currentUser.uid) && <span onClick={handleDelete} className="delete"><i className="bi bi-trash3"></i></span> }
         </p>
     </div>
   )
